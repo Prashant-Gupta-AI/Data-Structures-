00c3e3d1f39101c89bf1bca4b1328cea62bd6683
Prashant-Gupta-AI <99531500+Prashant-Gupta-AI@users.noreply.github.com>
2022-07-01 12:46:56 +0530

    Recursion

---
+++ LinkedListRecursion.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node* next;
+};
+void Print(struct Node* p)
+{
+    if(p==NULL) return;
+    Print(p->next);
+    printf("%d",p->data);
+    
+}
+struct Node* Insert(Node* head, int data){
+    Node *temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = data;
+    temp->next = NULL;
+    if(head == NULL) head = temp;
+    else{
+        Node* temp1 = head;
+        while(temp1->next != NULL) temp1 = temp1->next;
+        temp1->next = temp;
+    }
+    return head;
+}
+
+int main(){
+    struct Node* head = NULL;
+    head = Insert(head, 2);
+    head = Insert(head, 4);
+    head = Insert(head, 6);
+    head = Insert(head, 5);
+    Print(head);
+}
